ace98a00ae4f967eece211f4be75a51f9d4bea85
zazedd <leomendesantos@gmail.com>
2024-06-26 14:34:28 +0200

    added dockerfile, R script and nix shell

--- src/main.ml
+++ src/main.ml
@@ -1,19 +1,44 @@
-(* This is the main entry-point for the executable.
-   Edit [cmd] to set the text for "--help" and modify the command-line interface. *)
+open Lwt.Infix
 
 let () = Prometheus_unix.Logging.init ()
-
 let program_name = "example"
 
-let main repo =
-  let repo = Current_git.Local.v (Fpath.v repo) in
-  let engine = Current.Engine.create (Pipeline.v ~repo) in
-  let site = Current_web.Site.(v ~has_role:allow_all) ~name:program_name (Current_web.routes engine) in
-  Lwt_main.run begin
-    Lwt.choose [
-      Current.Engine.thread engine;  (* The main thread evaluating the pipeline. *)
-      Current_web.run site;    (* Optional: provides a web UI *)
-    ]
-  end
+let find_git_root dir =
+  let cmd = [| "git"; "-C"; dir; "rev-parse"; "--show-toplevel" |] in
+  Lwt_process.pread ("", cmd) >|= String.trim
 
-let () = main "." |> ignore
+let main config mode repo =
+  Lwt_main.run
+    ( find_git_root repo >>= fun repo ->
+      let repo = Current_git.Local.v (Fpath.v repo) in
+      let engine = Current.Engine.create ~config (Pipeline.v ~repo) in
+      let site =
+        Current_web.Site.(v ~has_role:allow_all)
+          ~name:program_name
+          (Current_web.routes engine)
+      in
+      Lwt.choose
+        [
+          Current.Engine.thread engine;
+          (* The main thread evaluating the pipeline. *)
+          Current_web.run ~mode site;
+          (* Optional: provides a web UI *)
+        ] )
+
+open Cmdliner
+
+let repo =
+  Arg.value
+  @@ Arg.pos 0 Arg.dir (Sys.getcwd ())
+  @@ Arg.info ~doc:"The directory containing the .git subdirectory." ~docv:"DIR"
+       []
+
+let cmd =
+  let doc = "Build the head commit of a local Git repository using Docker." in
+  let info = Cmd.info program_name ~doc in
+  Cmd.v info
+    Term.(
+      term_result
+        (const main $ Current.Config.cmdliner $ Current_web.cmdliner $ repo))
+
+let () = exit @@ Cmd.eval cmd
--- src/pipeline.ml
+++ src/pipeline.ml
@@ -1,7 +1,22 @@
+(*open Current.Syntax*)
 module Git = Current_git
+module Docker = Current_docker.Default
+
+let timeout = Duration.of_hour 1
+let pull = false
+
+let dockerfile =
+  `Contents
+    {|
+      FROM nixos/nix
+      COPY ./scripts/flake.nix flake.nix
+      COPY ./scripts/flake.lock flake.lock
+      RUN nix develop --extra-experimental-features 'nix-command flakes' 
+    |}
+  |> Current.return
 
 let v ~repo () =
-  (* Replace this with your actual pipeline: *)
+  Git.Local.repo repo |> Fpath.to_string |> Format.printf "%s@.";
   let src = Git.Local.head_commit repo in
-  Format.printf "hi@.";
-  Current.ignore_value src
+  let image = Docker.build ~dockerfile ~pull ~timeout (`Git src) in
+  Docker.run image ~args:[ "Rscript"; "--help" ]
