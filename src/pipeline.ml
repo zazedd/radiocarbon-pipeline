02e3c772ed42d89e01726761cbe0390216876080
zazedd <leomendesantos@gmail.com>
2024-06-26 11:35:39 +0200

    update2

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -3,5 +3,5 @@ module Git = Current_git
 let v ~repo () =
   (* Replace this with your actual pipeline: *)
   let src = Git.Local.head_commit repo in
-  (*Format.printf "hi@.";*)
+  Format.printf "hi@.";
   Current.ignore_value src
