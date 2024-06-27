c0734937f4be51f8bfc00078a36de5b7d6ce95f1
zazedd <leomendesantos@gmail.com>
2024-06-27 11:26:40 +0200

    Starting to port over to current_nix

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -1,13 +1,46 @@
-(*open Current.Syntax*)
+open Current.Syntax
+open Lwt.Infix
 module Git = Current_git
 module Nix = Current_nix.Default
 
 let timeout = Duration.of_hour 1
 let pull = false
+let last_commit : Git.Commit.t option ref = ref None
+
+let handle_repo_change src commit =
+  let config = Current.Config.now |> Current_incr.observe |> Option.get in
+  let job =
+    Current.Job.create
+      ~switch:(Current.Switch.create ~label:"test" ())
+      ~label:"Test" ~config ()
+  in
+  Current.Job.start ~timeout job ~level:Current.Level.Harmless >>= fun () ->
+  Git.with_checkout ~job commit @@ fun dir ->
+  last_commit := Some commit;
+  Nix.shell
+    ~args:
+      [
+        [
+          "Rscript"; "scripts/script.r"; "inputs/denmark.csv"; "outputs/out.csv";
+        ];
+        [ "echo"; "hello" ];
+      ]
+    ~timeout (`Git src)
+  >|= fun res -> res
 
 let v ~repo () =
   let src = Git.Local.head_commit repo in
-  Nix.build ~timeout (`Git src)
+  let+ commit = src in
+  handle_repo_change src commit
+(*Nix.shell*)
+(*  ~args:*)
+(*    [*)
+(*      [*)
+(*        "Rscript"; "scripts/script.r"; "inputs/denmark.csv"; "outputs/out.csv";*)
+(*      ];*)
+(*      [ "echo"; "hello" ];*)
+(*    ]*)
+(*  ~timeout (`Git src)*)
 
 (*
    TODO: 1
@@ -15,8 +48,8 @@ let v ~repo () =
    and check if there are new/modified files. If so, we should run them through our script.
 
    TODO: 2
-   The outputs currently stay inside the docker container, we should remove and place them inside
-   the outputs/ directory at some point.
+   The outputs currently stay inside the temporary folder, we should remove and place them inside
+   the outputs/ directory at some point, commit them and push them to the repo.
 
    TODO: 3
    Currently only a local repository is considered, we should consider remote repositories as well.
