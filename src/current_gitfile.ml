0e61c74609f9a63c8e2208ed9c84f7618bbe99d7
zazedd <leomendesantos@gmail.com>
2024-07-01 12:00:52 +0200

    Manual test

--- src/current_gitfile.ml
+++ src/current_gitfile.ml
@@ -252,7 +252,7 @@ module Raw = struct
 
     let git_cmd cmd args =
       let cmd = cmd |> command_to_str in
-      ("", Array.of_list (("git" :: [ cmd ]) @ args))
+      ("", Array.of_list (("pwd; git" :: [ cmd ]) @ args))
 
     module Key = struct
       type t = {
