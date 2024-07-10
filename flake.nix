3f15b7ed8a0843b4a8e1a82703486a8309ada394
zazedd <leomendesantos@gmail.com>
2024-07-10 15:53:01 +0200

    rm pkgs

--- flake.nix
+++ flake.nix
@@ -3,15 +3,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
-  outputs = { self, nixpkgs, nixpkgs-master, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-master, nix-filter, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
-            inherit system;
-          };
-          new-pkgs = import nixpkgs-master {
             inherit system;
           };
           ourRPackages = pkgs.rPackages.override (old: old // { overrides = {
@@ -32,31 +30,9 @@
 
           devShells.dev = mkShell {
             buildInputs = [ ocaml ];
-            packages = with nixpkgs; [ 
+            packages = [ 
               dune_3
               opam
-              ocamlPackages.bos
-              ocamlPackages.astring
-              ocamlPackages.bos
-              ocamlPackages.cmdliner
-              ocamlPackages.duration
-              ocamlPackages.logs
-              ocamlPackages.fpath
-              ocamlPackages.lwt
-              ocamlPackages.alcotest
-              ocamlPackages.ppx_deriving
-              ocamlPackages.prometheus
-              ocamlPackages.re
-              ocamlPackages.ppx_deriving_yojson
-              ocamlPackages.yojson
-              ocamlPackages.mirage-crypto
-              ocamlPackages.hex
-              new-pkgs.ocamlPackages.gitlab-unix
-              ocamlPackages.cohttp-lwt-unix
-              ocamlPackages.ptime
-              ocamlPackages.rresult
-              ocamlPackages.capnp
-              ocamlPackages.stdint
 
               capnproto
               sqlite
